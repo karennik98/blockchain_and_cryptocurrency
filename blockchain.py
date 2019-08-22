3b002daff3506c7c3d31a0fcceb3a8e359827f3f
karennik98 <karen.nikoghosyan.98@gmail.com>
2019-08-23 03:26:37 +0400

    Added broadcasting sync for blocks

--- blockchain.py
+++ blockchain.py
@@ -116,12 +116,15 @@ class Blockchain:
             proof += 1
         return proof
 
-    def get_balance(self):
+    def get_balance(self, sender=None):
         """Calculate and return the balance for a participant.
         """
-        if self.public_key == None:
-            return None
-        participant = self.public_key
+        if sender == None:
+            if self.public_key == None:
+                return None
+            participant = self.public_key
+        else:
+            participant = sender
         # Fetch a list of all sent coin amounts for the given person (empty lists are returned if the person was NOT the sender)
         # This fetches sent amounts of transactions that were already included in blocks of the blockchain
         tx_sender = [[tx.amount for tx in block.transactions
@@ -153,13 +156,13 @@ class Blockchain:
     # One required one (transaction_amount) and one optional one (last_transaction)
     # The optional one is optional because it has a default value => [1]
 
-    def add_transaction(self, recipient, sender, signature, amount=1.0):
+    def add_transaction(self, recipient, sender, signature, amount=2.0, is_receiving=False):
         """ Append a new value as well as the last blockchain value to the blockchain.
 
         Arguments:
             :sender: The sender of the coins.
             :recipient: The recipient of the coins.
-            :amount: The amount of coins sent with the transaction (default = 1.0)
+            :amount: The amount of coins sent with the transaction (default = 2.0)
         """
         # transaction = {
         #     'sender': sender,
@@ -172,21 +175,22 @@ class Blockchain:
         if Verification.verify_transaction(transaction, self.get_balance):
             self.__open_transactions.append(transaction)
             self.save_data()
-            for node in self.__peer_nodes:
-                url = 'http://{}/broadcast-transaction'.format(node)
-                try:
-                    json_data = {
-                        'sender': sender,
-                        'recipient': recipient,
-                        'amount': amount,
-                        'signature': signature
-                    }
-                    response = requests.post(url, json_data)
-                    if response.status_code == 400 or response.status_code == 500:
-                        print('Transaction declined, needs resolving.')
-                        return False
-                except requests.exceptions.ConnectionError:
-                    continue
+            if not is_receiving:
+                for node in self.__peer_nodes:
+                    url = 'http://{}/broadcast-transaction'.format(node)
+                    try:
+                        json_data = {
+                            'sender': sender,
+                            'recipient': recipient,
+                            'amount': amount,
+                            'signature': signature
+                        }
+                        response = requests.post(url, json_data)
+                        if response.status_code == 400 or response.status_code == 500:
+                            print('Transaction declined, needs resolving.')
+                            return False
+                    except requests.exceptions.ConnectionError:
+                        continue
             return True
         return False
 
@@ -218,8 +222,31 @@ class Blockchain:
         self.__chain.append(block)
         self.__open_transactions = []
         self.save_data()
+        for node in self.__peer_nodes:
+            url = 'http://{}/broadcast-block'.format(node)
+            converted_block = block.__dict__.copy()
+            converted_block['transactions'] = [tx.__dict__ for tx in converted_block['transactions']]
+            try:
+                response = requests.post(url, json={'block': converted_block})
+                if response.status_code == 400 or response.status_code == 500:
+                    print('Block declined, needs resolving.')
+            except requests.exceptions.ConnectionError:
+                continue
         return block
-
+    
+    
+    def add_block(self, block):
+        transactions = [Transaction(tx['sender'], tx['recipient'], tx['signature'], tx['amount']) for tx in block['transactions']]
+        proof_is_valid = Verification.valid_proof(transactions[:-1], block['previous_hash'], block['proof'])
+        hashes_match = hash_block(self.chain[-1]) == block['previous_hash']
+        if not proof_is_valid or not hashes_match:
+            return False
+        converted_block = Block(block['index'], block['previous_hash'], 
+                                transactions, block['proof'], block['timestamp'])
+        self.__chain.append(converted_block)
+        self.save_data()
+        return True
+        
 
     def add_peer_node(self, node):
         """ Adds a new node to the peer node set.
--- node.py
+++ node.py
@@ -73,6 +73,72 @@ def get_balance():
         return jsonify(responce), 500
 
 
+@app.route('/broadcast-transaction', methods=['POST'])
+def broadcast_transaction():
+    values = request.get_json()
+    if not values:
+        response = {
+            'message': 'No data found.'
+        }
+        return jsonify(response), 400
+    required = ['sender', 'recipient', 'amount', 'signature']
+    if not all(key in values for key in required):
+        response = {
+            'message': 'Some data is missing.'
+        }
+        return jsonify(response), 400
+    succeess = blockchain.add_transaction(values['sender'], values['recipient'], 
+                                          values['signature'], values['amount'], is_receiving=True)
+    if succeess:
+        response = {
+            'message': 'Successfully added transaction.',
+            'transaction': {
+                'sender': values['sender'],
+                'recipient': values['recipient'],
+                'amount': values['amount'],
+                'signature': values['signature']
+            }
+        }
+        return jsonify(response), 201
+    else:
+        response = {
+            'message': 'Creating a transaction failed.'
+        }
+        return jsonify(response), 500
+    
+@app.route('/broadcast-block', methods=['POST'])
+def broadcast_block():
+    values = request.get_json()
+    if not values:
+        response = {
+            'message': 'No data found.'
+        }
+        return jsonify(response), 400
+    if 'block' not in values:
+        response = {
+            'message': 'Some data is missing.'
+        }
+        return jsonify(response), 400
+    block = values['block']
+    if block['index'] == blockchain.chain[-1].index + 1:
+        if blockchain.add_block(block):
+            response = {
+                'message': 'Block added.'
+            }
+            return jsonify(response), 201
+        else:
+            response = {
+                'message': 'block seems invalid.'
+            }
+            return jsonify(response), 500
+    elif block['index'] > blockchain.chain[-1].index:
+        pass
+    else:
+        response = {
+            'message': 'Blockchain seems to be shorter, block not added.'
+        }
+        return jsonify(response), 409
+    
 
 @app.route('/transaction', methods=['POST'])
 def add_transaction():
